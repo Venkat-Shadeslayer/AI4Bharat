5e74691906dc3466cf058204e8b67654c85da6f3
Venkat-Shadeslayer <136975381+Venkat-Shadeslayer@users.noreply.github.com>
2023-10-04 01:48:58 +0530

    Add files via upload

--- pmindia.py
+++ pmindia.py
@@ -55,75 +55,59 @@ def scroll_to_bottom(driver):
         
 
 
-driver.get("https://www.pmindia.gov.in/en/news-updates/")
+langs_news_updates = ["en/news-updates/","hi/न्यूज-अपडेट्स/","asm/বাতৰি-সংজোযন/","bn/সর্বশেষ-প্রাপ্ত-সংবাদ/","gu/ન્યુઝ-અપડેટ/","kn/ಇತ್ತೀಚಿನ-ಸುದ್ದಿಗಳು/","ml/പുതിയ-വാർത്തകൾ/",
+         "mni/অনৌবা-পাউশিং/","mr/ताज्या-बातम्या/","ory/ସଦ୍ୟତମ-ଖବର/","pa/ਨਿਊਜ਼-ਅੱਪਡੇਟ/","ta/சமீபத்திய-செய்திகள்/","te/తాజా-స‌మాచారం/","ur/مختصر-تازہ-خبریں/"]
 
-scroll_to_bottom(driver)
-'''
-# Locate and click on the language dropdown
-Languages = driver.find_element(By.XPATH, '//*[@id="lang_choice_polylang-2"]')
-Languages.click()
+for lang in langs_news_updates:
 
-# Accept any alerts if they are present
-try:
-    alert = Alert(driver)
-    alert.accept()
-except:
-    pass
+    driver.get(f"https://www.pmindia.gov.in/{lang}/")
 
-class InfiniteScroll(object):
-    def __init__(self, last):
-        self.last = last
+    scroll_to_bottom(driver)
 
-    def __call__(self, driver):
-        new = driver.execute_script('return document.body.scrollHeight')
-        if new > self.last:
-            return new
-        else:
-            return False'''
 
-# Create a set to store unique URLs
-unique_urls = set()
+    # Create a set to store unique URLs
+    unique_urls = set()
 
-try:
-    last_height = driver.execute_script('return document.body.scrollHeight')
-    flag = 1
-    while flag == 1:
-        driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
-        try:
-            new_height = driver.execute_script("return document.body.scrollHeight")
-            if new_height != last_height:
-                last_height = new_height
-                time.sleep(2)
-            else:
-                print("End of page reached")
+    try:
+        last_height = driver.execute_script('return document.body.scrollHeight')
+        flag = 1
+        while flag == 1:
+            driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+            try:
+                new_height = driver.execute_script("return document.body.scrollHeight")
+                if new_height != last_height:
+                    last_height = new_height
+                    time.sleep(2)
+                else:
+                    print("End of page reached")
+                    flag = 0
+            except:
                 flag = 0
-        except:
-            flag = 0
 
-        # Find and extract all the links on the page
-        #links = [link.get_attribute('href') for link in driver.find_elements(By.TAG_NAME, 'a')]
-        links = driver.find_elements(By.XPATH,'//*[@id="wrapper"]/section[2]/div[1]/div/div[3]/ul/li/div[2]/h3/a')
-        article_links = [link.get_attribute('href') for link in links]
-        print (len(links))
+            # Find and extract all the links on the page
+            #links = [link.get_attribute('href') for link in driver.find_elements(By.TAG_NAME, 'a')]
+            links = driver.find_elements(By.XPATH,'//*[@id="wrapper"]/section[2]/div[1]/div/div[3]/ul/li/div[2]/h3/a')
+            article_links = [link.get_attribute('href') for link in links]
+            print (len(links))
 
-         # Filter out None values from the links list
-        #links = [link for link in links if link is not None]
+            # Filter out None values from the links list
+            #links = [link for link in links if link is not None]
 
 
-        # Add the links to the set (avoiding duplicates)
-        unique_urls.update(article_links)
+            # Add the links to the set (avoiding duplicates)
+            unique_urls.update(article_links)
 
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
-# Close the Selenium driver
-driver.quit()
+    # Close the Selenium driver
+    driver.quit()
 
-# Write unique URLs to a text file
-file_path = 'pmindia_unique_urls.txt'
-with open(file_path, 'w', encoding='utf-8') as file:
-    for url in unique_urls:
-        file.write(url + '\n')
+    # Write unique URLs to a text file
+    file_path = f'pmindia_unique_urls_{lang.split("/")[0]}_{lang.split("/")[1]}.txt'
+    with open(file_path, 'w', encoding='utf-8') as file:
+        for url in unique_urls:
+            file.write(url + '\n')
 
 
 
