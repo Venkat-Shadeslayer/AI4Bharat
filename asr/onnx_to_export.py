a789b7398e5cb890c12ec3f482716c438392877f
Venkat-Shadeslayer <136975381+Venkat-Shadeslayer@users.noreply.github.com>
2024-03-14 11:30:08 +0530

    Update onnx_to_export.py

--- asr/onnx_to_export.py
+++ asr/onnx_to_export.py
@@ -11,6 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
+
+#This script lets us to export a Nemo model to ONNX format.
+
 import argparse
 
 from nemo.collections.asr.models import EncDecClassificationModel, EncDecCTCModel, EncDecSpeakerLabelModel
